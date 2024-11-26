8d0732c366bca45f7c17c3e28e049d19fbdcffa7
EzequielSR <ezequieldesr@gmail.com>
2024-11-26 12:13:03 -0300

    Refatoração do Exercicio 10

---
+++ src/Vetores_e_Matrizes/Exercicio_10/SomaDiagonalPrincipal.java
@@ -0,0 +1,34 @@
+package Vetores_e_Matrizes.Exercicio_10;
+
+import java.util.Scanner;
+
+public class SomaDiagonalPrincipal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o tamanho da matriz quadrada: ");
+        int tamanho = scanner.nextInt();
+
+        int[][] matriz = new int[tamanho][tamanho];
+
+        System.out.println("Digite os elementos da matriz:");
+        for (int i = 0; i < tamanho; i++){
+            for (int j = 0; j < tamanho; j++){
+                System.out.println("Elemento [" + i + "][" + j + "]: ");
+                matriz[i][j] = scanner.nextInt();
+            }
+        }
+
+        int somaDiagonal = calcularSomaNaDiagonalPrincipal(matriz);
+        System.out.println("A soma da diagonal principal é: " + somaDiagonal);
+
+        scanner.close();
+    }
+    public static int calcularSomaNaDiagonalPrincipal(int[][] matriz){
+        int soma = 0;
+        for (int i = 0; i < matriz.length; i++){
+            soma += matriz[i][i];
+        }
+        return soma;
+    }
+}
