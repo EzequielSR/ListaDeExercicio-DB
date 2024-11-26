dea50f4dc84903a1ea78e1123d82de68f3dab10f
EzequielSR <ezequieldesr@gmail.com>
2024-11-26 14:06:34 -0300

    Refatoração do Exercicio 11

---
+++ src/Vetores_e_Matrizes/Exercicio_11/OrdernarVetor.java
@@ -0,0 +1,50 @@
+package Vetores_e_Matrizes.Exercicio_11;
+
+import java.util.Random;
+
+public class OrdernarVetor {
+    public static int[] gerarVetorAleatorio (int tamanho, int max){
+        int[] vetor = new int[tamanho];
+        Random random = new Random();
+
+        for (int i = 0; i < tamanho; i++){
+            vetor[i] = random.nextInt(max);
+        }
+        return vetor;
+    }
+
+    public static void bubbleSort(int[] vetor){
+        int n = vetor.length;
+        for (int i = 0; i < n - 1; i++){
+            for (int j = 0; j < n - i - 1; j++){
+                if(vetor[j] > vetor[j + 1]){
+                    int temp = vetor[j];
+                    vetor[j] = vetor[j + 1];
+                    vetor[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void imprimirVetor(int[] vetor){
+        for(int i = 0; i < vetor.length; i++){
+            System.out.print(vetor[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int tamanho = 100;
+        int max = 100;
+
+        int[] vetor = gerarVetorAleatorio(tamanho,max);
+
+        System.out.println("Vetor original:");
+        imprimirVetor(vetor);
+
+        bubbleSort(vetor);
+
+        System.out.println("Vetor ordenado:");
+        imprimirVetor(vetor);
+    }
+}
