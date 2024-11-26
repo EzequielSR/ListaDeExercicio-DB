d99a482952f7bc81516b55a415e8938269b357cb
EzequielSR <ezequieldesr@gmail.com>
2024-11-26 11:13:42 -0300

    Refatoração do Exercício 6

---
+++ src/Estrutura_de_Repetição/Exercicio_6/NumerosAleatorios.java
@@ -0,0 +1,31 @@
+/* 6) Gere 10 números aleatórios entre 0 e 100; mostre todos na tela
+(em uma única linha); identifique o menor e o maior deles. */
+
+// Resolução:
+
+package Estrutura_de_Repetição.Exercicio_6;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class NumerosAleatorios {
+
+    public static void main(String[] args) {
+        List<Integer> numeros = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < 10; i++){
+            numeros.add(random.nextInt(101));
+        }
+
+        System.out.println("Números: " + numeros);
+
+        int menor = Collections.min(numeros);
+        int maior = Collections.max(numeros);
+
+        System.out.println("Menor número: " + menor);
+        System.out.println("Maior número: " + maior);
+    }
+}
