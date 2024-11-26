0815e6f67435e68b50efd3cdf7fcf38f88f637e1
EzequielSR <ezequieldesr@gmail.com>
2024-11-25 21:54:10 -0300

    Resolução Exercício 5

---
+++ src/Estrutura_de_Repetição/Exercicio5/Tabuada.java
@@ -0,0 +1,18 @@
+package Estrutura_de_Repetição.Exercicio5;
+
+import java.util.Scanner;
+
+public class Tabuada{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número para a tabuada: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Tabuada do número " + numero + ":");
+        for (int i =1; i<=10; i++){
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+        scanner.close();
+    }
+}
